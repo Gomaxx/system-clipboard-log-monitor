e3a246a1468d0d7a448069b24a1b8b74a8b54979
Gomaxx <oma.one@gmail.com>
2020-08-21 13:39:34 +0800

    功能调整。

--- src/cc/eoma/clipboard/SystemClipboardMonitor.java
+++ src/cc/eoma/clipboard/SystemClipboardMonitor.java
@@ -1,5 +1,7 @@
 package cc.eoma.clipboard;
 
+import cc.eoma.clipboard.example.ImageSelection;
+
 import java.awt.*;
 import java.awt.datatransfer.*;
 import java.io.FileWriter;
@@ -21,6 +23,25 @@ public class SystemClipboardMonitor implements ClipboardOwner {
 
     @Override
     public void lostOwnership(Clipboard clipboard, Transferable contents) {
+        if (clipboard.isDataFlavorAvailable(DataFlavor.imageFlavor)) {
+            try {
+                /*
+                    如果是 IMAGE 信息时，在VirtualBox虚拟机中粘帖会报错，导致VirtualBox虚拟机异常关闭（在 其他地方未发现类似问题）;
+                    因此检测到图片时，延迟5s（以便VritualBox虚拟机中粘帖图像）后在将剪切板重置为自己所有，便于后续监听剪切板信息变更。
+
+                    另：使用 JHotKeys 监听 CTRL+C时，会阻断 系统 复制 动作，导致系统剪切板中不会存在要复制的信息（复制动作失效）。
+                 */
+                Thread.sleep(5000);
+
+                Image image = (Image) clipboard.getData(DataFlavor.imageFlavor);
+                ImageSelection imageSelection = new ImageSelection(image);
+                clipboard.setContents(imageSelection, this);
+            } catch (Exception e) {
+                clipboard.setContents(contents, this);
+            }
+            return;
+        }
+
         if (!clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {
             clipboard.setContents(contents, this);
             return;
